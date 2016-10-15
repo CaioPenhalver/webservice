8b85138e3511aed8ee1eb96c2b091d60a0e90690
Caio Penhalver <cpenhalver@gmail.com>
2016-10-14 21:45:47 -0300

    Calcular frete

--- src/br/com/transportadora/CalculoDeFrete.java
+++ src/br/com/transportadora/CalculoDeFrete.java
@@ -2,12 +2,13 @@ package br.com.transportadora;
 
 public class CalculoDeFrete {
 
-	public double calcularFrete(Frete frete){
-		
-		return 0;
+	public double calcularFrete(Frete frete){ 
+		return (frete.getNumeroDeProdutos() * 25) 
+				+ (frete.getValorTotalDaRemessa() * 0.01)
+				+ calularImpostos(); 
 	}
 	
 	private double calularImpostos(){
-		return 1;
-	}
+		return 13;
+	} 
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -7,7 +7,6 @@ import br.com.transportadora.TransportadoraUtil;
 public class Test {
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		
 		String token = TransportadoraUtil.criarToken("caio");
 		System.out.println(token);
 		String decodedToken = TransportadoraUtil.decodeToken(token);
